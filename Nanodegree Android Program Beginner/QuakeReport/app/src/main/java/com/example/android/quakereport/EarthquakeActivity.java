fb3619a75a7d17e66c1f264e174caf782646f217
jennifernghinguyen <jennifernghi@yahoo.com>
2017-01-10 22:10:03 -0800

    QuakeReport setting menu

--- Nanodegree Android Program Beginner/QuakeReport/app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ Nanodegree Android Program Beginner/QuakeReport/app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -27,6 +27,8 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -127,6 +129,23 @@ public class EarthquakeActivity extends AppCompatActivity
         // TODO: Loader reset, so we can clear out our existing data.
         mAdapter.clear();
         Log.i(LOG_TAG, "in onLoaderReset()");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 
